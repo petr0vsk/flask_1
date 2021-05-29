0b6a9f50f5f8ac08411ce5c86d7e37082d1d9ae9
petr0vsk <petrovskjy.aleksander@gmail.com>
2021-05-29 17:07:39 +0300

    debug mode

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def index():
     return '<h1>Hello</h1>'
@@ -12,3 +11,7 @@ def index():
 @app.route('/<name>')
 def name(name):
     return f'<h1>Hello  {name} </h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
