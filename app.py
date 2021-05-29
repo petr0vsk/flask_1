cb1a96bf3b30c620a1b2f41aabd1ac64f6fee9ce
petr0vsk <petrovskjy.aleksander@gmail.com>
2021-05-29 16:55:19 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello</h1>'
+
+@app.route('/<name>')
+def name(name):
+    return f'<h1>Hello  {name} </h1>'
